66ee951268f184f39a3534add5cbf9088f395bb7
hitsubunnu <hitsu@cpan.org>
2010-10-13 06:25:22 +0000

    Import of HITSU/Cache-Memcached-Fast-CGI-0.05 from CPAN.
    gitpan-cpan-distribution: Cache-Memcached-Fast-CGI
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         HITSU/Cache-Memcached-Fast-CGI-0.05.tar.gz
    gitpan-cpan-author:       HITSU
    gitpan-cpan-maturity:     released

--- lib/Cache/Memcached/Fast/CGI.pm
+++ lib/Cache/Memcached/Fast/CGI.pm
@@ -9,11 +9,11 @@ Cache::Memcached::Fast::CGI - Capture the STDOUT for Memcached in a pure cgi pro
 
 =head1 VERSION
 
-Version 0.03
+Version 0.05
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.05';
 
 use Cache::Memcached::Fast;
 use IO::Capture::Stdout;
